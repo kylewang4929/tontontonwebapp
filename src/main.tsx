d50918d64b9c2e53eb0d05cb08140a38ac477298
王锐彬 <492960423@qq.com>
2024-05-19 21:22:49 +0800

    1

--- src/main.tsx
+++ src/main.tsx
@@ -42,7 +42,7 @@ const router = createBrowserRouter(
       <Route path="game" element={<Game />} />
     </Route>
   ),
-  {basename: '/tontontonwebapp'}
+  // {basename: '/tontontonwebapp'}
 );
 
 
